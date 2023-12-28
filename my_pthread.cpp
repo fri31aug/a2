dd9b091d7b08151966f57bdd7b0a2102771ee34d
Ananya <96532503+fri31aug@users.noreply.github.com>
2023-12-28 15:39:15 -0700

    Update my_pthread.cpp

--- my_pthread.cpp
+++ my_pthread.cpp
@@ -8,18 +8,18 @@ pthread_mutex_t lock;
 void *mythread(void *arg) {
     long thread_id = reinterpret_cast<long>(arg);
 
-    pthread_mutex_lock(&lock);  
+    pthread_mutex_lock(&lock);  // Lock the mutex
 
     std::cout << "thread " << thread_id << ": begin (number = " << number << ")" << std::endl;
     number *= 2;
+    // Print after doubling
     std::cout << "thread " << thread_id << ": done (number = " << number << ")" << std::endl;
 
-    pthread_mutex_unlock(&lock);  
+    pthread_mutex_unlock(&lock);  // Unlock the mutex
     return NULL;
 }
 
 int main() {
-    // Initialize the mutex
     if (pthread_mutex_init(&lock, NULL) != 0) {
         std::cerr << "Mutex init has failed" << std::endl;
         return 1;
@@ -27,29 +27,25 @@ int main() {
 
     std::cout << "main: begin (number = " << number << ")" << std::endl;
 
+    // Array of threads
     pthread_t threads[3];
 
     for (long i = 0; i < 3; ++i) {
         if (pthread_create(&threads[i], NULL, &mythread, reinterpret_cast<void *>(i + 1)) != 0) {
-            std::cerr << "Error creating thread " << i << std::endl;
+            std::cerr << "Error creating thread " << i + 1 << std::endl;
             return 1;
         }
     }
 
-    
-    mythread(reinterpret_cast<void *>(0));
-
-    
     for (int i = 0; i < 3; ++i) {
         if (pthread_join(threads[i], NULL) != 0) {
-            std::cerr << "Error joining thread " << i << std::endl;
+            std::cerr << "Error joining thread " << i + 1 << std::endl;
             return 1;
         }
     }
 
     std::cout << "main: end (number = " << number << ")" << std::endl;
 
-   
     pthread_mutex_destroy(&lock);
 
     return 0;
